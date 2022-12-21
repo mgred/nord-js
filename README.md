56c989325ac9746ce6501f4a2fe4bdd6ea3a4a2f
Marc Redemske <github@mgred.dev>
2022-12-21 21:10:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nord JS
+
+A [Nord theme](https://www.nordtheme.com/) port for JavaScript/TypeScript
+
+## Installation
+
+```bash
+npm i --save nord-js
+```
+
+## Usage
+
+### CommonJS/Node
+
+```javascript
+const Color = require("nord-js");
+```
+
+### ESM/TS
+
+```javascript
+import * as Color from "nord-js";
+```
+
+### Browser
+
+```html
+<script src="/path/to/index.js"></script>
+```
+
+## License
+
+MIT
---
+++ index.ts
@@ -0,0 +1,26 @@
+// Nord Color Palette
+// https://www.nordtheme.com/docs/colors-and-palettes
+
+// Polar Night
+export const NORD0 = "#2E3440";
+export const NORD1 = "#3B4252";
+export const NORD2 = "#434C5E";
+export const NORD3 = "#4C566A";
+
+// Snow Storm
+export const NORD4 = "#D8DEE9";
+export const NORD5 = "#E5E9F0";
+export const NORD6 = "#ECEFF4";
+
+// Frost
+export const NORD7 = "#8FBCBB";
+export const NORD8 = "#88C0D0";
+export const NORD9 = "#81A1C1";
+export const NORD10 = "#5E81AC";
+
+// Aurora
+export const NORD11 = "#BF616A";
+export const NORD12 = "#D08770";
+export const NORD13 = "#EBCB8B";
+export const NORD14 = "#A3BE8C";
+export const NORD15 = "#B48EAD";
